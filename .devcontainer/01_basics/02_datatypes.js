9430652463b75b2144f75f248063fdcbfa299dee
ajaytiwari5 <145701880+ajaytiwari5@users.noreply.github.com>
2024-05-13 06:54:37 +0000

    02_datatype

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";   // treat all js code as new version
+
+// alert( 3+3 )  // we are using nodejs not browser
+
+// console.log(3+3); console.log('ajay') //code must be readable not write this type
+
+// ; not neccesary to used its
+
+/* datatype- number- range is 2 the power of 53 if no. is greater then we use bigint
+
+string=> ""  
+boolean=> true/ false
+null=> standlone value // represntation empty if we want to temrature but system not worked properly then its return null not zero
+undefined=> not defined yet
+symbol=> for finding uniqueness
+object
+*/
+
+console.log(typeof "hitesh");
+
+console.log(typeof null);
+
+// null is a object 
+
+console.log(undefined);
