003cf4ffb624eb68c68c443879f76af01cdea92c
ajaytiwari5 <145701880+ajaytiwari5@users.noreply.github.com>
2024-05-13 07:23:57 +0000

    03

---
+++ .devcontainer/01_basics/03_conersionopration.js
@@ -0,0 +1,26 @@
+let score = "33rtt";
+
+console.log(typeof score);   //same
+console.log(typeof(score));  //same
+
+let valueInNumber = Number(score)  // for changing score type string to number 
+
+console.log(typeof valueInNumber);  //it shows type of number
+
+console.log(valueInNumber);   // it shows NaN means not a number bcz type change but value not exist
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)  // for changing 1 in boolean means true
+console.log(booleanIsLoggedIn);
+
+/* 1=> true  0=> false
+""=> false
+"ajay"=> true
+
+we also change number in string using string keyword
+*/
+
+
+
+
+
