8c2aba8de5c62d7594c6c46e40632ad2f6c82e3a
ajaytiwari5 <145701880+ajaytiwari5@users.noreply.github.com>
2024-05-13 12:02:57 +0000

    04

---
+++ .devcontainer/01_basics/04_operation.js
@@ -0,0 +1,48 @@
+let value= 3
+let negValue = -value   //for getting negative value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);   //all of basic operation
+
+let str1 = "hi"
+let str2 = "ajay"
+let str3 = str1 + str2   // only we can add not subtract string
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);  // if first is string type then operation is string type
+console.log(1 + 2 + "2");
+
+//console.log((3+ 2)(*( 5 - 4) +3))    //are not clarify and not true
+
+// 18,19,20,21,23 are not officialy used 
+
+console.log(+true);  //it means we increment but here its not possible output 1
+console.log(true+);  // its eror bcz it nt consider
+console.log(+"");     // output is 1 0 bcz empty means false means 0
+
+/*let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2
+
+basically we dont define variable that type it is not readable and also not correct path
+
+*/
+
+let gameCounter = 100;
+gameCounter++;        //we also write ++gameCounter
+console.log(gameCounter);  //101
+
+
+
+
+
+
+
+
